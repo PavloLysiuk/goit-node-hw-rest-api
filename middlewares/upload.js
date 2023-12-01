8990c09f7550ea59130eba9f53b40684a8814667
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-12-01 17:53:23 +0200

    add public and temp, create upload middleware

---
+++ middlewares/upload.js
@@ -0,0 +1,34 @@
+import multer from 'multer';
+import path from 'path'
+import { HttpError } from '../helpers/index.js';
+
+const destination = path.resolve('temp');
+
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, file, cb) => {
+    const uniquePrefix = `${Date.now()}_${Math.round() * 1e9}}`;
+    const fileName = `${uniquePrefix}_${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const limits = {
+  fileSize: 5 * 1024 * 1024,
+};
+
+const fileFilter = (req, file, cb) => {
+  const extension = file.originalname.split('.').pop();
+  if (extension === 'exe') {
+    return cb(HttpError(400, 'Invalid file extension'));
+  }
+  cb(null, true);
+};
+
+const upload = multer({
+  storage,
+  limits,
+  // fileFilter,
+});
+
+export default upload;
